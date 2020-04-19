f34c614c73f9fc23e56bb19ef60f9d0036b4d120
Negin Namiranian <negin.namiranian@gmail.com>
2020-04-18 17:19:37 -0700

    Add project files.

---
+++ ASP.NET_CRUD_ENTITY/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_CRUD_ENTITY.Models;
+
+namespace ASP.NET_CRUD_ENTITY.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View("Index",GetEmployeeList());
+        }
+        public ActionResult AddOrEditEmployee(Employee emp)
+        {
+            
+                using (mvcCrudDb db = new mvcCrudDb())
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                }
+            return Content("Success");
+        }
+        public ActionResult RenderAddEmployee()
+        {
+            Employee emp = new Employee();
+            return PartialView("Add",emp);
+        }
+        public List<Employee> GetEmployeeList()
+        {
+            using (mvcCrudDb db = new mvcCrudDb())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return empList;
+            }
+        }
+    }
+}
---
+++ ASP.NET_CRUD_ENTITY/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_CRUD_ENTITY.MvcApplication" Language="C#" %>
